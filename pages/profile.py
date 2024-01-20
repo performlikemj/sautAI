16be9c3b0ef96d24cc1d34457ed49b841a6e604d
MJ <mikey.j.1228@gmail.com>
2024-01-20 23:35:15 +0900

    Update authentication logic and chat functionality

--- pages/profile.py
+++ pages/profile.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import os
 import datetime
-from utils import api_call_with_refresh
+from utils import api_call_with_refresh, is_user_authenticated
 
 
 def profile():
@@ -20,159 +20,155 @@ def profile():
         else:
             user_data = {}
 
-        # Define a container for each field
-        username_container = st.container()
-        email_container = st.container()
-        phone_container = st.container()
-        diet_container = st.container()
-        allergy_container = st.container()
-        goal_container = st.container()
+        if is_user_authenticated():
+            # Define a container for each field
+            username_container = st.container()
+            email_container = st.container()
+            phone_container = st.container()
+            diet_container = st.container()
+            allergy_container = st.container()
+            goal_container = st.container()
 
-        # Goal management section
-        with goal_container:
-            st.header("Manage Your Goals")
-            # Fetch current goal
-            goal_response = api_call_with_refresh(
-                url=f'{os.getenv("DJANGO_URL")}/customer_dashboard/api/user_goal/',
-                method='get',
-                headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
-            )
-            current_goal = goal_response.json() if goal_response.status_code == 200 else {}
-            goal_name = st.text_input("Goal Name", value=current_goal.get('goal_name', ''))
-            goal_description = st.text_area("Goal Description", value=current_goal.get('goal_description', ''))
-
-
-            if st.button("Update Goal"):
-                goal_data = {
-                    "goal_name": goal_name,
-                    "goal_description": goal_description
-                }
-                update_goal_response = api_call_with_refresh(
-                    url=f'{os.getenv("DJANGO_URL")}/customer_dashboard/api/goal_management/',
-                    method='post',
-                    data=goal_data,
+            # Goal management section
+            with goal_container:
+                st.header("Manage Your Goals")
+                # Fetch current goal
+                goal_response = api_call_with_refresh(
+                    url=f'{os.getenv("DJANGO_URL")}/customer_dashboard/api/user_goal/',
+                    method='get',
                     headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
                 )
-                if update_goal_response.status_code == 200:
-                    st.success("Goal updated successfully!")
-                else:
-                    st.error("Failed to update goal.")    
+                current_goal = goal_response.json() if goal_response.status_code == 200 else {}
+                goal_name = st.text_input("Goal Name", value=current_goal.get('goal_name', ''))
+                goal_description = st.text_area("Goal Description", value=current_goal.get('goal_description', ''))
 
-        # Username field
-        with username_container:
-            username = st.text_input("Username", value=user_data.get('username', ''))
-            if st.button("Update Username"):
-                update_response = api_call_with_refresh(
-                    url=f'{os.getenv("DJANGO_URL")}/customer_dashboard/auth/api/update_profile/',
-                    method='post',
-                    data={'username': username},
-                    headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
-                )
-                if update_response.status_code == 200:                    
-                    st.success("Username updated successfully!")
 
-        # Email field
-        with email_container:
-            email = st.text_input("Email", value=user_data.get('email', ''))
-            if st.button("Update Email"):
-                update_response = api_call_with_refresh(
-                    url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
-                    method='post',
-                    data={'email': email},
-                    headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
-                )
-                if update_response.status_code == 200:
-                    st.success("Email updated successfully!")
+                if st.button("Update Goal"):
+                    goal_data = {
+                        "goal_name": goal_name,
+                        "goal_description": goal_description
+                    }
+                    update_goal_response = api_call_with_refresh(
+                        url=f'{os.getenv("DJANGO_URL")}/customer_dashboard/api/goal_management/',
+                        method='post',
+                        data=goal_data,
+                        headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
+                    )
+                    if update_goal_response.status_code == 200:
+                        st.success("Goal updated successfully!")
+                    else:
+                        st.error("Failed to update goal.")    
 
-        # Phone number field
-        with phone_container:
-            phone_number = st.text_input("Phone Number", value=user_data.get('phone_number', ''))
-            if st.button("Update Phone Number"):
-                update_response = api_call_with_refresh(
-                    url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
-                    method='post',
-                    data={'phone_number': phone_number},
-                    headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
-                )
-                if update_response.status_code == 200:
-                    st.success("Phone number updated successfully!")
+            # Username field
+            with username_container:
+                username = st.text_input("Username", value=user_data.get('username', ''))
+                if st.button("Update Username"):
+                    update_response = api_call_with_refresh(
+                        url=f'{os.getenv("DJANGO_URL")}/customer_dashboard/auth/api/update_profile/',
+                        method='post',
+                        data={'username': username},
+                        headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
+                    )
+                    if update_response.status_code == 200:                    
+                        st.success("Username updated successfully!")
 
-        # Dietary preference field
-        with diet_container:
-            dietary_preferences = ['Vegan', 'Vegetarian', 'Pescatarian', 'Gluten-Free', 'Keto', 'Paleo', 'Halal', 'Kosher', 'Low-Calorie', 'Low-Sodium', 'High-Protein', 'Dairy-Free', 'Nut-Free', 'Raw Food', 'Whole 30', 'Low-FODMAP', 'Diabetic-Friendly', 'Everything']
+            # Email field
+            with email_container:
+                email = st.text_input("Email", value=user_data.get('email', ''))
+                if st.button("Update Email"):
+                    update_response = api_call_with_refresh(
+                        url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
+                        method='post',
+                        data={'email': email},
+                        headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
+                    )
+                    if update_response.status_code == 200:
+                        st.success("Email updated successfully!")
 
-            user_dietary_pref = user_data.get('dietary_preference', '')
-            index = 0  # Default index
-            if user_dietary_pref in dietary_preferences:
-                index = dietary_preferences.index(user_dietary_pref)
+            # Phone number field
+            with phone_container:
+                phone_number = st.text_input("Phone Number", value=user_data.get('phone_number', ''))
+                if st.button("Update Phone Number"):
+                    update_response = api_call_with_refresh(
+                        url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
+                        method='post',
+                        data={'phone_number': phone_number},
+                        headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
+                    )
+                    if update_response.status_code == 200:
+                        st.success("Phone number updated successfully!")
 
-            dietary_preference = st.selectbox("Dietary Preference", dietary_preferences, index=index)
-            if st.button("Update Dietary Preference"):
-                update_response = api_call_with_refresh(
-                    url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
-                    method='post',
-                    data={'dietary_preference': dietary_preference},
-                    headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
-                )
-                if update_response.status_code == 200:                    
-                    st.success("Dietary preference updated successfully!")
-        with allergy_container:
-            allergies = [
-                'Peanuts', 
-                'Tree nuts', 
-                'Milk', 
-                'Egg', 
-                'Wheat', 
-                'Soy', 
-                'Fish', 
-                'Shellfish', 
-                'Sesame', 
-                'Mustard', 
-                'Celery', 
-                'Lupin', 
-                'Sulfites', 
-                'Molluscs', 
-                'Corn', 
-                'Gluten', 
-                'Kiwi', 
-                'Latex', 
-                'Pine Nuts', 
-                'Sunflower Seeds', 
-                'Poppy Seeds', 
-                'Fennel', 
-                'Peach', 
-                'Banana', 
-                'Avocado', 
-                'Chocolate', 
-                'Coffee', 
-                'Cinnamon', 
-                'Garlic', 
-                'Chickpeas', 
-                'Lentils', 
-                'None'
-            ]
-            user_allergy = user_data.get('allergy', '')
-            index = 0
-            if user_allergy in allergies:
-                index = allergies.index(user_allergy)
-            allergy = st.selectbox("Allergy", allergies, index=index)
-            if st.button("Update Allergy"):
-                update_response = api_call_with_refresh(
-                    url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
-                    method='post',
-                    data={'allergy': allergy},
-                    headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
-                )
-                if update_response.status_code == 200:                    
-                    st.success("Allergy updated successfully!")
+            # Dietary preference field
+            with diet_container:
+                dietary_preferences = ['Vegan', 'Vegetarian', 'Pescatarian', 'Gluten-Free', 'Keto', 'Paleo', 'Halal', 'Kosher', 'Low-Calorie', 'Low-Sodium', 'High-Protein', 'Dairy-Free', 'Nut-Free', 'Raw Food', 'Whole 30', 'Low-FODMAP', 'Diabetic-Friendly', 'Everything']
+
+                user_dietary_pref = user_data.get('dietary_preference', '')
+                index = 0  # Default index
+                if user_dietary_pref in dietary_preferences:
+                    index = dietary_preferences.index(user_dietary_pref)
+
+                dietary_preference = st.selectbox("Dietary Preference", dietary_preferences, index=index)
+                if st.button("Update Dietary Preference"):
+                    update_response = api_call_with_refresh(
+                        url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
+                        method='post',
+                        data={'dietary_preference': dietary_preference},
+                        headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
+                    )
+                    if update_response.status_code == 200:                    
+                        st.success("Dietary preference updated successfully!")
+            with allergy_container:
+                allergies = [
+                    'Peanuts', 
+                    'Tree nuts', 
+                    'Milk', 
+                    'Egg', 
+                    'Wheat', 
+                    'Soy', 
+                    'Fish', 
+                    'Shellfish', 
+                    'Sesame', 
+                    'Mustard', 
+                    'Celery', 
+                    'Lupin', 
+                    'Sulfites', 
+                    'Molluscs', 
+                    'Corn', 
+                    'Gluten', 
+                    'Kiwi', 
+                    'Latex', 
+                    'Pine Nuts', 
+                    'Sunflower Seeds', 
+                    'Poppy Seeds', 
+                    'Fennel', 
+                    'Peach', 
+                    'Banana', 
+                    'Avocado', 
+                    'Chocolate', 
+                    'Coffee', 
+                    'Cinnamon', 
+                    'Garlic', 
+                    'Chickpeas', 
+                    'Lentils', 
+                    'None'
+                ]
+                user_allergy = user_data.get('allergy', '')
+                index = 0
+                if user_allergy in allergies:
+                    index = allergies.index(user_allergy)
+                allergy = st.selectbox("Allergy", allergies, index=index)
+                if st.button("Update Allergy"):
+                    update_response = api_call_with_refresh(
+                        url=f'{os.getenv("DJANGO_URL")}/auth/api/update_profile/',
+                        method='post',
+                        data={'allergy': allergy},
+                        headers={'Authorization': f'Bearer {st.session_state.user_info["access"]}'}
+                    )
+                    if update_response.status_code == 200:                    
+                        st.success("Allergy updated successfully!")
     else:
         # User is not logged in, display a message or redirect
         st.warning("Please log in to view and update your profile.")
-
-
-
-    # Similar for other fields
-    # ...
 
 if __name__ == "__main__":
     profile()
